9434ac507ed369c43499a5b1498d73f576a3997f
BruceGeng2012 <79483662+BruceGeng2012@users.noreply.github.com>
2024-08-22 18:35:28 +0800

    Update ScriptletInjDemo.js

--- ScriptletInjDemo.js
+++ ScriptletInjDemo.js
@@ -1,2 +1,17 @@
-/// ScriptletInjDemo.js
-console.log("Hello I'm running via scriptlet injection.");
+
+        // 获取所有的 div 元素
+        const divs = document.getElementsByTagName('div');
+
+        // 检查是否有至少7个 div 元素
+        if (divs.length >= 7) {
+            // 选择第7个 div （注意索引是从0开始的）
+            const seventhDiv = divs[6];
+            
+            // 获取第七个 div 的父节点
+            const parentElement = seventhDiv.parentNode;
+            
+            // 从 DOM 中删除第七个 div
+            parentElement.removeChild(seventhDiv);
+        } else {
+            console.log("There are not enough div elements to remove the seventh one.");
+        }
