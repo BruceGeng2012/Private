42be2d9ef5e15cf09fb87b170f63e10a0bdb848f
BruceGeng2012 <79483662+BruceGeng2012@users.noreply.github.com>
2024-08-22 19:53:20 +0800

    Update my-scriptlets.js

--- my-scriptlets.js
+++ my-scriptlets.js
@@ -1,9 +1,10 @@
-// 获取所有具有指定类名的div元素
-const divs = document.querySelectorAll('.example');
-
-// 删除第8到47个div元素
-for (let i = 7; i < 47; i++) {
-    if (divs[i]) {
-        divs[i].parentNode.removeChild(divs[i]);
+function removeSpecificDivs() {
+    let divs = document.querySelectorAll('div[style*="z-index:100"]');
+    for (let i = 7; i < 47; i++) {
+        if (divs[i]) {
+            divs[i].parentNode.removeChild(divs[i]);
+        }
     }
 }
+
+removeSpecificDivs();
