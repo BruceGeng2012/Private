c4eba4c6cd0909243a915b1c68cc5ddd93450f4d
BruceGeng2012 <79483662+BruceGeng2012@users.noreply.github.com>
2024-08-22 19:42:52 +0800

    Update my-scriptlets.js

--- my-scriptlets.js
+++ my-scriptlets.js
@@ -1,17 +1,9 @@
+// 获取所有具有指定类名的div元素
+const divs = document.querySelectorAll('.example');
 
-        // 获取所有的 div 元素
-        const divs = document.getElementsByTagName('div');
-
-        // 检查是否有至少7个 div 元素
-        if (divs.length >= 7) {
-            // 选择第7个 div （注意索引是从0开始的）
-            const seventhDiv = divs[6];
-            
-            // 获取第七个 div 的父节点
-            const parentElement = seventhDiv.parentNode;
-            
-            // 从 DOM 中删除第七个 div
-            parentElement.removeChild(seventhDiv);
-        } else {
-            console.log("There are not enough div elements to remove the seventh one.");
-        }
+// 删除第8到47个div元素
+for (let i = 7; i < 47; i++) {
+    if (divs[i]) {
+        divs[i].parentNode.removeChild(divs[i]);
+    }
+}
